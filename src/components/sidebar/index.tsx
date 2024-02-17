90d78d29a90bcecff77bf84ae601e093f33490c0
Majid Hasan <majidhasan@Majids-MacBook-Pro.local>
2024-02-17 23:12:18 +0600

    Sidebar Done

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,5 +1,6 @@
 import { getAuthUserDetails } from "@/lib/queries";
 import React from "react";
+import MenuOptions from "./menu-options";
 
 type Props = {
   id: string;
@@ -22,9 +23,50 @@ const Sidebar = async ({ id, type }: Props) => {
 
   if (!details) return;
 
-  let sideBarLogo = user.Agency.agencyLogo || "/assets/plura-logo";
+  let sideBarLogo = user.Agency.agencyLogo || "/assets/plura-logo.svg";
 
-  return <div>Sidebar</div>;
+  if (!isWhileLabeledAgency) {
+    if (type === "subaccount") {
+      sideBarLogo =
+        user.Agency.SubAccount.find((subaccount) => subaccount.id === id)
+          ?.subAccountLogo || user.Agency.agencyLogo;
+    }
+  }
+
+  const sidebarOpt =
+    type === "agency"
+      ? user.Agency.SidebarOption || []
+      : user.Agency.SubAccount.find((subaccount) => subaccount.id === id)
+          ?.SidebarOption || [];
+
+  const subaccounts = user.Agency.SubAccount.filter((subaccount) =>
+    user.Permissions.find(
+      (permission) =>
+        permission.subAccountId === subaccount.id && permission.access
+    )
+  );
+
+  return (
+    <>
+      <MenuOptions
+        defaultOpen={true}
+        details={details}
+        id={id}
+        sidebarLogo={sideBarLogo}
+        sidebarOpt={sidebarOpt}
+        subAccounts={subaccounts}
+        user={user}
+      />
+      <MenuOptions
+        details={details}
+        id={id}
+        sidebarLogo={sideBarLogo}
+        sidebarOpt={sidebarOpt}
+        subAccounts={subaccounts}
+        user={user}
+      />
+    </>
+  );
 };
 
 export default Sidebar;
